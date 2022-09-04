88d8a46f899940be4c61f8d66dca123aa9bf49d5
Femi Adegoke <femiadegoke@Samuels-MacBook-Pro-2.local>
2022-09-04 23:49:15 +0100

    UI Updated

--- src/components/EditArticle/index.js
+++ src/components/EditArticle/index.js
@@ -63,7 +63,7 @@ const EditArticle = ({ show, handleClose, data }) => {
                 Close
             </Button>
             <Button variant="warning" onClick={onSaveClicked}>
-                Edit changes
+                Save Changes
             </Button>
             </Modal.Footer>
         </Modal>
